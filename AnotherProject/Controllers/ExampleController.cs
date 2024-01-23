36e52af1e9f51932e3ff659404f124b780b79178
Christine <Christine@CHRISTINE_PC>
2024-01-23 15:29:08 -0500

    Add project files.

---
+++ AnotherProject/Controllers/ExampleController.cs
@@ -0,0 +1,54 @@
+﻿using Antlr.Runtime.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AnotherProject.Controllers
+{
+    public class ExampleController : ApiController
+    {
+
+        /// <summary>
+        /// This method will return the value -1
+        /// </summary>
+        /// <returns>
+        /// -1
+        /// </returns>
+        /// <example>
+        /// GET: localhost:xx/api/example => -1
+        /// </example>
+        public int Get()
+        {
+            return -1;
+        }
+
+
+        /// <summary>
+        /// Receives an input number and returns that number divided by 2. Truncates the result to an integer
+        /// </summary>
+        /// <param name="id">The input number</param>
+        /// <returns>
+        /// The number divided by 2.
+        /// </returns>
+        /// <example>
+        /// GET: localhost:xx/api/example/8 => 4
+        /// </example>
+        /// <example>
+        /// GET: localhost:xx/api/example/10 => 5
+        /// </example> 
+        /// <example>
+        /// GET: localhost:xx/api/example/-16 => -8
+        /// </example> 
+        /// <example>
+        /// GET: localhost:xx/api/example/-11 => -5
+        /// </example> 
+        public int Get(int id)
+        {
+            int result = id / 2;
+            return result;
+        }
+    }
+}
---
+++ AnotherProject/Controllers/StringController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AnotherProject.Controllers
+{
+    public class StringController : ApiController
+    {
+        //GOAL: configure a certain response for an input HTTP request
+
+        //what the input is going to look like
+        // GET localhost:xx/api/string/{sean}
+
+        //the output is going to look like
+        // "Hello sean!"
+
+        public string Get(string id)
+        {
+            string message = "Hello " + id + "!";
+            return message;
+        }
+
+
+        //GOAL:
+        //method which serves:
+        //POST localhost:xx/api/string => "Post Request!"
+
+        public string Post()
+        {
+            return "Post Request!";
+        }
+
+
+    }
+}
